9ca21533fd2485f1166ca7dbf2f8600e3f028e4b
Abarna S <abarna.s@basiscloudsolutions.com>
2024-12-20 19:15:40 +0530

    [worked on the register page]

---
+++ src/Page/CommonStyles.js
@@ -0,0 +1,80 @@
+import { makeStyles } from "@mui/styles";
+
+export const commonStyles = makeStyles({
+  formContainer: {
+    height: "100vh",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    "& .MuiButton-root": {
+      textTransform: "none",
+    },
+  },
+  formBox: {
+    border: "1px solid #ccc",
+    padding: "30px",
+    borderRadius: "10px",
+    width: "90%",
+    maxWidth: "400px",
+    height: "auto",
+    maxHeight: "90%",
+    minWidth: "300px",
+    margin: "auto",
+    "@media (max-width: 600px)": {
+      width: "95%",
+      padding: "15px",
+    },
+    "@media (min-width: 1200px)": {
+      width: "30%",
+    },
+  },
+  formTitle: {
+    textAlign: "center",
+    marginBottom: "20px",
+    fontSize: "32px !important",
+  },
+  formInputFields: {
+    "& .MuiFormLabel-root": {
+      fontSize: "16px",
+      marginBottom: "10px",
+      fontWeight: "bold",
+    },
+
+    "& .MuiOutlinedInput-input": {
+      padding: "10px",
+      width: "calc(400px - 30px)",
+    },
+    marginBottom: "20px",
+  },
+  formButton: {
+    width: "calc(400px - 10px)",
+    "& .MuiButton-root": {
+      width: "100%",
+      backgroundColor: "#ff5e33",
+      color: "#fff",
+      textTransform: "none",
+      padding: "10px !important",
+      fontSize: "16px !important",
+    },
+  },
+  resendOtpContainer: {
+    "& .MuiButton-root": {
+      textTransform: "none",
+      padding: "0px 10px !important",
+    },
+    "& .MuiButton-root:hover": {
+      backgroundColor: "transparent !important",
+    },
+
+    textAlign: "end",
+    marginTop: "20px",
+  },
+  registerButton: {
+    textAlign: "center",
+    marginTop: "20px",
+    "& .MuiButton-root:hover": {
+      backgroundColor: "transparent !important",
+    },
+  },
+});
--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,112 +1,91 @@
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
 import React from "react";
 import makeStyles from "@mui/styles/makeStyles";
-import { Button, InputLabel, TextField, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { commonStyles } from "./CommonStyles";
 
 const useStyles = makeStyles({
-  loginContainer: {
-    height: "100vh",
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  loginBox: {
-    border: "1px solid #ccc",
-    padding: "30px",
-    borderRadius: "10px",
-    width: "90%",
-    maxWidth: "400px",
-    height: "auto",
-    maxHeight: "90%",
-    minWidth: "300px",
-    margin: "auto",
-    "@media (max-width: 600px)": {
-      width: "95%",
-      padding: "15px",
-    },
-    "@media (min-width: 1200px)": {
-      width: "30%",
-    },
-  },
-  loginTitle: {
-    textAlign: "center",
-    marginBottom: "20px",
-    fontSize: "32px",
-  },
-  inputFields: {
-    "& .MuiFormLabel-root": {
-      fontSize: "16px",
-      marginBottom: "10px",
-      fontWeight: "bold",
-    },
-
-    "& .MuiOutlinedInput-input": {
-      padding: "10px",
-      width: "calc(400px - 30px)",
-    },
-    marginBottom: "20px",
-  },
-  loginButton: {
-    width: "calc(400px - 10px)",
-    "& .MuiButton-root": {
-      width: "100%",
-      backgroundColor: "#ff5e33",
-      color: "#fff",
-      textTransform: "none",
-      padding: "10px !important",
-    },
-  },
   resendOtpContainer: {
     "& .MuiButton-root": {
       textTransform: "none",
       padding: "0px 10px !important",
     },
-    "& .MuiButton-root::hover": {
+    "& .MuiButton-root:hover": {
       backgroundColor: "transparent !important",
     },
     textAlign: "end",
     marginTop: "20px",
   },
+  registerButton: {
+    textAlign: "center",
+    marginTop: "20px",
+    "& .MuiButton-root:hover": {
+      backgroundColor: "transparent !important",
+    },
+  },
 });
 
 const Login = () => {
   const classes = useStyles();
+  const commonClasses = commonStyles();
+  const navigate = useNavigate();
   const initialState = {
     mobileNo: "",
     otpNo: "",
   };
   const [state, setState] = React.useState(initialState);
   const { mobileNo, otpNo } = state;
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setState({ ...state, [name]: value });
+  };
+
+  console.log(state);
+
+  const handleRegister = () => {
+    navigate("/register");
+  };
+
   return (
-    <Box className={classes.loginContainer}>
-      <Box className={classes.loginBox}>
-        <Typography variant="h1" className={classes.loginTitle}>
+    <Box className={commonClasses.formContainer}>
+      <Box className={commonClasses.formBox}>
+        <Typography variant="h1" className={commonClasses.formTitle}>
           Login
         </Typography>
-        <Box className={classes.inputFields}>
+        <Box className={commonClasses.formInputFields}>
           <InputLabel>Mobile No</InputLabel>
           <TextField
             variant="outlined"
             name="mobileNo"
             placeholder="Enter Mobile No"
+            type="number"
             value={mobileNo}
+            onChange={handleInputChange}
           />
         </Box>
-        <Box className={classes.inputFields}>
+        <Box className={commonClasses.formInputFields}>
           <InputLabel>OTP No</InputLabel>
           <TextField
             variant="outlined"
             name="otpNo"
             placeholder="Enter OTP No"
             value={otpNo}
+            onChange={handleInputChange}
           />
           <Box className={classes.resendOtpContainer}>
             <Button>Resend OTP</Button>
           </Box>
         </Box>
-        <Box className={classes.loginButton}>
+        <Box className={commonClasses.formButton}>
           <Button>Login</Button>
+        </Box>
+        <Box className={classes.registerButton}>
+          <Button onClick={handleRegister}>Register Here</Button>
         </Box>
       </Box>
     </Box>
